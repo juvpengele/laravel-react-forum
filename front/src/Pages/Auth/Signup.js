7419e1ce371142d59538e192c1172eecf4c59f01
Juvénal Pengele <pengeleglodiejuvenal@gmail.com>
2019-07-18 17:32:40 +0000

    [Client] Add feature to handle errors and show feedback  in the registration form

--- front/src/Pages/Auth/Signup.js
+++ front/src/Pages/Auth/Signup.js
@@ -12,7 +12,16 @@ class SignUp extends React.Component {
             email: '',
             password: '',
             password_confirmation: '',
-            FormErrors: new Errors()
+            errors: {
+                name: [],
+                email: [],
+                password: [],
+                password_confirmation: [],
+            },
+            feedback: {
+                show: false,
+                message: ''
+            }
         }
     }
 
@@ -26,7 +35,6 @@ class SignUp extends React.Component {
         this.setState({
             email: event.target.value
         });
-
     }
 
     handlePasswordChange(event) {
@@ -37,7 +45,7 @@ class SignUp extends React.Component {
 
     handlePasswordConfirmationChange(event) {
         this.setState({
-            password_confirmation: event.target.value
+            password_confirmation: event.target.value,
         });
     }
 
@@ -64,16 +72,41 @@ class SignUp extends React.Component {
 
         axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+        this.setState({
+            errors:  {
+                name: [],
+                email: [],
+                password: [],
+                password_confirmation: [],
+            }
+        });
+
+
         axios.post(endpoint, data, {
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
             }
         })
-        .then((response) => console.log(response))
+        .then((response) => {
+            this.setState({
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: '',
+                feedback: {
+                    show: true,
+                    message: 'You are registered successfully !'
+                }
+            })
+        })
         .catch(error =>{
             if(error.response.data) {
-               
+                this.setState((prevState) => {
+                    return {
+                       errors: {...prevState.errors, ...error.response.data.errors }
+                    }
+                });
             }
         });
 
@@ -84,29 +117,47 @@ class SignUp extends React.Component {
             <div className="row my-4">
                 <form className="col-md-6 row mx-auto" onSubmit={(event) => this.handleFormSubmit(event)} method="POST">
                     <h2 className="text-center col-md-12 mt-2">Sign up</h2>
+
+                    {
+                        this.state.feedback.show &&
+                        <div className="alert alert-dismissible alert-success col-md-12 mt-2">
+                            <button type="button" className="close" data-dismiss="alert">&times;</button>
+                            <strong>Well done!</strong> { this.state.feedback.message }
+                        </div>
+                    }
+
                     <div className="form-group col-md-12">
                         <label htmlFor="name">Your name</label>
                         <input type="text" className="form-control" id="name" aria-describedby="emailHelp"
                                placeholder="Enter your name" onChange={(event) => this.handleNameChange(event)} value={this.state.name}/>
 
-                        {/*<small id="emailHelp" className="form-text text-muted">*/}
-                        {/*Lorem*/}
-                        {/*</small>*/}
+                        { this.state.errors['name'][0] &&
+                            <small id="emailHelp" className="form-text text-danger">
+                                { this.state.errors['name'][0] }
+                            </small>
+                        }
 
                     </div>
                     <div className="form-group col-md-12">
                         <label htmlFor="exampleInputEmail1">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                                placeholder="Enter email" onChange={(event) => this.handleEmailChange(event)} value={this.state.email} />
-                        {/*<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone*/}
-                        {/*else.*/}
-                        {/*</small>*/}
+                        { this.state.errors['email'][0] &&
+                        <small id="emailHelp" className="form-text text-danger">
+                            { this.state.errors['email'][0] }
+                        </small>
+                        }
                     </div>
                     <div className="form-group col-md-12">
                         <label htmlFor="password">Password</label>
                         <input type="password" className="form-control" id="password" placeholder="Enter your password"
                                value={this.state.password} onChange={ (event) => this.handlePasswordChange(event) }
                         />
+                        { this.state.errors['password'][0] &&
+                        <small id="emailHelp" className="form-text text-danger">
+                            { this.state.errors['password'][0] }
+                        </small>
+                        }
                     </div>
                     <div className="form-group col-md-12">
                         <label htmlFor="password_confirmation">Password (Confirmation)</label>
@@ -114,6 +165,11 @@ class SignUp extends React.Component {
                                placeholder="Re-enter your password"
                                value={ this.state.password_confirmation } onChange={ (event) => this.handlePasswordConfirmationChange(event) }
                         />
+                        { this.state.errors['password_confirmation'][0] &&
+                        <small id="emailHelp" className="form-text text-danger">
+                            { this.state.errors['password_confirmation'][0] }
+                        </small>
+                        }
                     </div>
                     <div className="col-md-12">
                         <button type="submit" className="btn btn-outline-info btn-lg">Sign up</button>
